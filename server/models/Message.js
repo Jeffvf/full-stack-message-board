6b397b56d49e7a9f239c3df1b3e49cebe6d431f1
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-29 20:46:37 -0300

    feat: model para mensagem

---
+++ server/models/Message.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, maxLength: 40 },
+  text: { type: String, required: true, maxLength: 300 },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+});
+
+MessageSchema.virtual('url').get(function() {
+  return `/message/${this._id}`;
+})
+
+const Message = mongoose.model('Message', MessageSchema);
+export default Message;
