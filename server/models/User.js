a8ee68e36487daf3f825ed5256ff6befdf38a4db
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-30 14:12:06 -0300

    refactor: campo username como unico

--- server/models/User.js
+++ server/models/User.js
@@ -5,13 +5,13 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   firstName: { type: String, required: true, maxLength: 35 },
   lastName: { type: String, required: true, maxLength: 35 },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   member: { type: Boolean, default: false }
 });
 
 UserSchema.virtual('url').get(function() {
-  return `/user/${this._id}`;
+  return `/users/${this._id}`;
 })
 
 const User = mongoose.model('User', UserSchema);
