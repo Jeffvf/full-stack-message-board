76f0ac92d2212bacb9a392fcd7deb2a35615100b
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-30 15:02:01 -0300

    feat: adiciona rotas de usuario na aplicacao

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import helmet from "helmet";
 import mongoose from "mongoose";
 import morgan from "morgan";
 
+import userRoutes from "./routers/users.js";
+
 dotenv.config();
 
 const app = express();
@@ -14,11 +16,16 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan('common'));
 
+app.use('/users', userRoutes);
+
 const PORT = process.env.PORT || 8000;
 
 const mongoDB = process.env.MONGODB_URL || process.env.MONGODB_DEV_URL;
+
+mongoose.set('strictQuery', false);
 mongoose.connect(mongoDB, { 
-  useNewUrlParser: true, useUnifiedTopology: true 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
 }).then(() => {
   app.listen(PORT, () => console.log('Listening on port: '+ PORT));
 }).catch((error) => {
