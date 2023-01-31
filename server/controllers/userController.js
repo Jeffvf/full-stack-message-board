7a59a830069bc47fe6d49f1666a131ae098014d1
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-31 20:19:01 -0300

    feat: update de informacoes do usuario

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -70,7 +70,7 @@ export const userCreatePost = [
       res.status(400).json({ errors: errors.array(), user});
     }
     if(userExists !== null){
-      res.status(400).json({ errors: 'Nome de usuário já utilizado', userExists });
+      res.status(400).json({ errors: 'Nome de usuário já utilizado', user });
     }
     user.save((err) => {
       if(err){
@@ -79,15 +79,68 @@ export const userCreatePost = [
       res.status(200).json(user);
     })
   }
-]
+];
 
 export const userUpdateGet = async(req, res) => {
-  res.status(404).json({ message: 'Não implementado' });
+  try {
+    const user = await User.findById(req.params.id, "-password");
+    res.status(200).json({ user: user });
+  } catch(err) {
+    res.status(404).json({ message: err.message });
+  }
 }
 
-export const userUpdatePost = async(req, res) => {
-  res.status(404).json({ message: 'Não implementado' });
-}
+export const userUpdatePost = [
+  body('firstName', 'Campo nome não deve ser vazio')
+  .trim()
+  .isLength({ min:2 })
+  .escape(),
+  body('lastName', 'Campo sobrenome não deve ser vazio')
+  .trim()
+  .isLength({ min:2 })
+  .escape(),
+  body('username', 'Email inválido')
+  .normalizeEmail()
+  .isEmail()
+  .escape(),
+  body('password', 'Campo senha deve possuir ao menos 8 caracteres')
+  .optional({ checkFalsy: true })
+  .trim()
+  .isLength({ min:8 })
+  .escape(),
+  body('confirmPassword', 'Campos de senha devem ser iguais')
+  .custom((value, { req }) => value === req.body.password),
+  async (req, res) => {
+    const errors = validationResult(req);
+    let salt, passwordHash, userExists;
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      _id: req.params.id
+    });
+
+    if(!errors.isEmpty()){
+      res.status(400).json({ errors: errors.array(), user });
+    }
+    if(req.body.password) {
+      try {
+        salt = await bcrypt.genSalt();
+        passwordHash = await bcrypt.hash(req.body.password, salt);
+        userExists = await User.findOne({ username: req.body.username })
+        user['password'] = passwordHash 
+      } catch(err) {
+        res.status(400).json({ errors: err });
+      }
+    }
+    User.findByIdAndUpdate(req.params.id, user, {}, (err, doc) => {
+      if(err){
+        res.status(400).json({ message: err })
+      }
+      res.redirect(doc.url)
+    });
+  }
+];
 
 export const userDeleteGet = async(req, res) => {
   res.status(404).json({ message: 'Não implementado' });
