6c41e1c91e24ff1761dc5405976456e300340dd5
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-01-30 23:07:43 -0300

    refactor: url da mensagem

--- server/models/Message.js
+++ server/models/Message.js
@@ -11,7 +11,7 @@ const MessageSchema = new Schema({
 });
 
 MessageSchema.virtual('url').get(function() {
-  return `/users/${this.user}/message/${this._id}`;
+  return `/message/${this._id}`;
 })
 
 const Message = mongoose.model('Message', MessageSchema);
