b1e49dd1653b0433ae269adba442939ddcdf83a2
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-05 17:33:00 -0300

    feat: exibe mensagens postadas

---
+++ client/src/app/messages/messages.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Message } from '../message';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+  messages: Message[] = [];
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getMessages();
+  }
+
+  getMessages(): void {
+    this.messageService.getMessages()
+      .subscribe(messages => this.messages = messages);
+  }
+}
