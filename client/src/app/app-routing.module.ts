ac65882e8fb624d5ec257cddc631526275f5436e
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-05 16:50:00 -0300

    feat: adiciona rotas a aplicacao

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MessagesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
