19588d25d28410f954c5e7c3012ad760783fa7ae
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-06 13:59:17 -0300

    feat: service para login de usuario

---
+++ client/src/app/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { UserCredentials, AuthUser } from './user';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  
+  private userUrl = "http://localhost:8000/users"
+
+  constructor(private http: HttpClient) { }
+
+  login(user: UserCredentials): Observable<AuthUser>{
+    return this.http.post<AuthUser>(`${this.userUrl}/login`, user)
+      .pipe(
+        catchError(err => of({
+            token: '',
+            user: '',
+            errors: err.error.errors
+          } as unknown as AuthUser)
+        )
+      )
+  }
+}
