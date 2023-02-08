7079630f6f2b37fff432081e523882f9c939c616
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-07 21:11:25 -0300

    feat: importa BrowserAnimationsModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -16,6 +17,7 @@ import { MessageDetailsComponent } from './message-details/message-details.compo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
