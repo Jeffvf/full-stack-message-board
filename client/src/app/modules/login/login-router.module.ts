29d7cef35c62146cdb8b2da15e0ae2d8e6613f70
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-08 14:06:36 -0300

    refactor: rota para login

--- client/src/app/modules/login/login-router.module.ts
+++ client/src/app/modules/login/login-router.module.ts
@@ -5,11 +5,7 @@ import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: [
-      {path: '', redirectTo: 'login', pathMatch: 'full'},
-      {path: 'login', component: LoginComponent}
-    ]
+    path: '', component: LoginComponent
   }
 ];
 
