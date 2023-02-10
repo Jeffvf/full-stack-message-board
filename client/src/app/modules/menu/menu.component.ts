d574dfa210ff0bbc08d454338c33a987480bd319
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-10 10:25:21 -0300

    feat: atualiza menu apos login

--- client/src/app/modules/menu/menu.component.ts
+++ client/src/app/modules/menu/menu.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
 
-  constructor(private router: Router) {}
+  user!: User | null;
+
+  constructor(private router: Router, private tokenStorage: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.user = this.tokenStorage.getUser();
+  }
 
   loginRedirect() {
     this.router.navigate(['/login'])
+  }
+
+  signOut(){
+    this.tokenStorage.signOut();
+    window.location.reload();
   }
 }
