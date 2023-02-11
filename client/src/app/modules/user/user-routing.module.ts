11edad0e469afbdf308a3c6f54f6bee520fa1c02
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-11 14:23:06 -0300

    feat: adiciona rota de update

--- client/src/app/modules/user/user-routing.module.ts
+++ client/src/app/modules/user/user-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './user-details.component';
+import { UserUpdateComponent } from './user-update.component';
 
 
 
 const routes: Routes = [
   {
-    path: 'user/:id', component: UserDetailsComponent
+    path: 'user/:id',
+    children: [
+      { path: '', component: UserDetailsComponent },
+      { path: 'update', component: UserUpdateComponent }
+    ]
   }
 ];
 
