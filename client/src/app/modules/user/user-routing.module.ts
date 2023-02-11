badfb37fbfd693184b03f0f29f666a37a2df26cc
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-11 11:59:24 -0300

    feat: modulo para usuario

---
+++ client/src/app/modules/user/user-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './user-details.component';
+
+
+
+const routes: Routes = [
+  {
+    path: 'user/:id', component: UserDetailsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
---
+++ client/src/app/modules/user/user.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FieldsetModule } from 'primeng/fieldset';
+import { AvatarModule } from 'primeng/avatar';
+
+import { UserRoutingModule } from './user-routing.module';
+import { UserDetailsComponent } from './user-details.component';
+
+
+@NgModule({
+  declarations: [
+    UserDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    FieldsetModule,
+    UserRoutingModule,
+    AvatarModule
+  ]
+})
+export class UserModule { }
