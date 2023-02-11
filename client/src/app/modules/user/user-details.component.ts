2aaf8b567f5806e981cebfadce007f3e9ba49e7b
Jefferson França <jefferson.vieiraf@hotmail.com>
2023-02-11 12:00:18 -0300

    feat: exibe pagina de detalhes do usuario

---
+++ client/src/app/modules/user/user-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { User, UserDetail } from "src/app/models/user";
+import { UserService } from "src/app/services/user.service";
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+
+export class UserDetailsComponent implements OnInit{
+
+  userDetail!: UserDetail;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getUser()
+  }
+
+  getUser(){
+    const id = String(this.route.snapshot.paramMap.get('id'));
+    this.userService.getUser(id)
+      .subscribe(user => this.userDetail = user);
+    console.log(this.user)
+  }
+
+  get user() { return this.userDetail.user }
+
+  get messages() { return this.userDetail.messages }
+}
